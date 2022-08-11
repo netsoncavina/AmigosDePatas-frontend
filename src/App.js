5a99d159573571611ac68fbadb7a58cec3e7470b
Netson <netsoncavina@gmail.com>
2022-08-11 00:20:24 -0300

    organização de pastas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
-
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Projeto Integrador
+        </Typography>
+        <img src="http://bit.ly/memories_image" alt="logo" height="80" />
+      </AppBar>
+      <Grow in={true}>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} md={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
