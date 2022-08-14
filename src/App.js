ab015272b68d16a65a77b436fb9d379ceb819163
Netson <netsoncavina@gmail.com>
2022-08-14 11:06:46 -0300

    adição do Browser Router

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from "react-redux";
-import { getPosts } from "./actions/posts";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Container } from "@material-ui/core";
+
 import Navbar from "./components/Navbar/Navbar";
-import useStyles from "./styles";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [dispatch]);
-
   return (
-    <Container maxwidth="lg">
-      <Navbar />
-
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            className={classes.mainContainer}
-            justifyContent="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} md={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxwidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
+        {/* <Home /> */}
+      </Container>
+    </BrowserRouter>
   );
 };
 
