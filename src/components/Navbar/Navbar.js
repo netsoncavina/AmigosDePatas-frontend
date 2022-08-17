1abcd693444c74ff808aa6f6f41966141afa29c2
Netson <netsoncavina@gmail.com>
2022-08-16 21:20:39 -0300

    adicionado controle de login

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AppBar, Toolbar, Typography, Avatar, Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import useStyles from "./styles";
@@ -8,7 +8,12 @@ import catImage from "../../images/cat.png";
 
 const Navbar = () => {
   const classes = useStyles();
-  const user = null;
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  useEffect(() => {
+    const token = user?.token;
+
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, []);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
