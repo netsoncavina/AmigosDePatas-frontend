b000ccf892e86e2be001d1301db5ca923a3bc6ad
Netson <netsoncavina@gmail.com>
2022-08-19 15:23:22 -0300

    botões são exibidos de acordo com usuario logado

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -8,7 +8,8 @@ import {
   Typography,
   Button,
 } from "@material-ui/core";
-import ThumbAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
@@ -18,6 +19,23 @@ import { deletePost, likePost } from "../../../actions/posts";
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
+  const Likes = () => {
+    if (post.likes.length > 0) {
+      return (
+        <>
+          <ThumbUpAltIcon fontSize="small" />
+          {post.likes.length}
+        </>
+      );
+    }
+    return (
+      <>
+        <ThumbUpAltOutlined fontSize="small" />
+      </>
+    );
+  };
+
   return (
     <Card className={classes.card}>
       <CardMedia
@@ -36,17 +54,21 @@ const Post = ({ post, setCurrentId }) => {
       <div className={classes.overlay2}>
         <Typography variant="body2">{post.localization}</Typography>
       </div>
-      <div className={classes.overlay3}>
-        <Button
-          style={{ color: "white" }}
-          size="small"
-          onClick={() => {
-            setCurrentId(post._id);
-          }}
-        >
-          <MoreHorizIcon fontSize="medium" />
-        </Button>
-      </div>
+      {user?.result?._id === post.owner ||
+      user?.result?.email === post.owner ||
+      user?.result?.sub === post.owner ? (
+        <div className={classes.overlay3}>
+          <Button
+            style={{ color: "white" }}
+            size="small"
+            onClick={() => {
+              setCurrentId(post._id);
+            }}
+          >
+            <MoreHorizIcon fontSize="medium" />
+          </Button>
+        </div>
+      ) : null}
       <div className={classes.details}>
         <Typography variant="body2" color="textSecondary">
           {post.age} anos
@@ -67,24 +89,27 @@ const Post = ({ post, setCurrentId }) => {
         <Button
           size="small"
           color="primary"
+          disabled={!user?.result}
           onClick={() => {
             dispatch(likePost(post._id));
           }}
         >
-          <ThumbAltIcon fontSize="small" />
-          {/* Curtir */}
-          {post.likeCount}
+          <Likes />
         </Button>
-        <Button
-          size="small"
-          color="primary"
-          onClick={() => {
-            dispatch(deletePost(post._id));
-          }}
-        >
-          <DeleteIcon fontSize="small" />
-          Delete
-        </Button>
+        {user?.result?._id === post.owner ||
+        user?.result?.email === post.owner ||
+        user?.result?.sub === post.owner ? (
+          <Button
+            size="small"
+            color="primary"
+            onClick={() => {
+              dispatch(deletePost(post._id));
+            }}
+          >
+            <DeleteIcon fontSize="small" />
+            Delete
+          </Button>
+        ) : null}
       </CardActions>
     </Card>
   );
