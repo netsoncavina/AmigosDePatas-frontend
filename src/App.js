09762f47a3df38a68eae4b2500b0c254c7413785
Netson <netsoncavina@gmail.com>
2022-08-19 15:54:38 -0300

    adaptação do componente

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Container } from "@material-ui/core";
 import Navbar from "./components/Navbar/Navbar";
@@ -8,17 +8,25 @@ import Auth from "./components/Auth/Auth";
 import "dotenv/config";
 
 const App = () => {
-  // const clientId = process.env.GOOGLE_ID;
   const clientId =
     "993941346806-5e42if1ru5hgl6t0t95cs13fkg3jtfll.apps.googleusercontent.com";
+
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   return (
     <GoogleOAuthProvider clientId={`${clientId}`}>
       <BrowserRouter>
-        <Container maxwidth="lg">
+        <Container maxwidth="xl">
           <Navbar />
           <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/auth" exact component={Auth} />
+            <Route path="/" exact component={() => <Redirect to="/posts" />} />
+            <Route path="/posts" exact component={Home} />
+            <Route path="/posts/search" exact component={Home} />
+            <Route
+              path="/auth"
+              exact
+              component={() => (!user ? <Auth /> : <Redirect to="/posts" />)}
+            />
           </Switch>
         </Container>
       </BrowserRouter>
