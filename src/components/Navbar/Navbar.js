363161eb92413b2b62779a3c4dfd9d5a05f36767
Netson <netsoncavina@gmail.com>
2022-08-19 15:31:16 -0300

    adicionada função de logout caso token esteja expirado

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { AppBar, Toolbar, Typography, Avatar, Button } from "@material-ui/core";
 import { Link, useHistory, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
 import useStyles from "./styles";
 
 import dogImage from "../../images/dog.png";
@@ -21,6 +22,13 @@ const Navbar = () => {
   };
   useEffect(() => {
     const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+      if (decodedToken.exp * 1000 < new Date().getTime()) {
+        logout();
+      }
+    }
 
     setUser(JSON.parse(localStorage.getItem("profile")));
   }, [location]);
