0ad82cfc5ae0674bf808e732467dc7bfc7b354db
Netson Cavina <netsoncavina@gmail.com>
2023-01-13 16:01:19 -0300

    mudança na url da api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://serveramigosdepatas.herokuapp.com/",
+  baseURL: "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {
